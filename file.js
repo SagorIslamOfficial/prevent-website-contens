1acf2ee6fed3f389f2ef51c62cb6b33305e13ef3
Sagor Islam <alaminislam1274@gmail.com>
2021-06-08 20:46:03 +0600

    Create JS file

---
+++ file.js
@@ -0,0 +1,35 @@
+//Punish the thief:
+
+(function () { 
+    const rgx = new RegExp(/(domainName)|(:portNumUsedInLocalDev)|/g); 
+    const host = window.location.host; 
+    const isMatch = !host.match(rgx); 
+ 
+    function showWarning() { 
+        let warning = document.createElement('h1'); 
+        warning.innerText = 'DON\'T STEAL'; 
+        warning.style.fontSize = '5em'; 
+        warning.style.fontWeight = 700; 
+        warning.style.position = 'fixed'; 
+        warning.style.left = `${(window.innerWidth/2)}px`; 
+        warning.style.top =`${ window.innerHeight /2}px`; 
+        warning.style.zIndex = 9999; 
+        document.body.appendChild(warning); 
+    } 
+ 
+    function exactRvg () { 
+        cornify_add(); 
+ 
+            window.setTimeout(()=> { 
+                exactRvg(); 
+            }, 500); 
+ 
+    } 
+    if (isMatch ) { 
+        showWarning(); 
+        $.getScript('https://www.cornify.com/js/cornify.js',function(){ 
+            exactRvg(); 
+        }); 
+    } 
+ 
+})(); 
